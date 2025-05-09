ca81bcafa0965ff1d000a68b9b637b37c0df77ee
wEPac <35659294+wEPac@users.noreply.github.com>
2025-05-09 05:50:23 +0200

    Update BMP_Utils.h

--- src/BMP_Utils.h
+++ src/BMP_Utils.h
@@ -1,18 +1,10 @@
 #ifndef   BMP_UTILS_H
 #define   BMP_UTILS_H
 
+#define BMP_RECORD_TO     1   // 0:TFT, 1:Sprite, 2:SPIFF
+#define BMP_SWAP_COLOR_BYTES  // uncomment to swap MSB<->LSB bytes for end result
+//#define BMP_SHOW_TIME         // uncomment to print how long it took to decode
 
-
-//*
-#define TFT_CS    18
-#define SD_CS      3
-
-void selectCSl(int num)
-{
-  digitalWrite(TFT_CS, !(num == TFT_CS));
-  digitalWrite(SD_CS,  !(num == SD_CS));
-}
-//*/
 
 uint16_t BMPread16(fs::File &f)
 {
@@ -34,102 +26,135 @@ uint32_t BMPread32(fs::File &f)
 
 void BMPdraw(const char *filename, int16_t x, int16_t y)
 { // draw a 16 or 24 bits Bitmap with NO alpha to display
-  //if ((x >= tft.width()) || (y >= tft.height())) return;
+  //if ((x >= tft.width()) || (y >= tft.height())) return;  // 1st filter, may be muted comment
 
-  File bitmap;
-  bitmap =   SD.open(filename, FILE_READ);    // Open requested file on SD card
+  File        bitmap;
+  bitmap =    SD.open(filename, FILE_READ);   // Open requested file from SD card
   if (!bitmap)
   {
-    //Serial.println("File not found\n");
-    //spr.println("File not found\n");
-    spr.print("File '"); spr.print(filename); spr.println("' not found\n");
+    //Serial.printf("File % not found\n", filename);
+    #if   (BMP_RECORD_TO == 0)
+    tft.printf("File %s not found\n", filename);
+    #elif (BMP_RECORD_TO == 1)
+    spr.printf("File %s not found\n", filename);
+    #elif (BMP_RECORD_TO == 2)
+    #endif // BMP_RECORD_TO
     return;
   }
-  //Serial.println("File found\n");
-  //spr.println("File found\n"); while(1) return;
-  //spr.print("File "); spr.print(filename); spr.println(" found\n"); while(1) return;
-
+  
   uint32_t    seekOffset;
   uint16_t    w, h, row, col, rgb, signature, depth, field;
   uint8_t     r, g, b;
-  //uint32_t    startTime =     millis();
+  #ifdef BMP_SHOW_TIME
+  uint32_t    startTime =   millis();
+  #endif // BMP_SHOW_TIME
 
   // bitmap header
-  signature =       BMPread16(bitmap);  // Signature
+  signature =               BMPread16(bitmap);  // Signature
   BMPread32(bitmap);                    // skip File Size
   BMPread32(bitmap);                    // skip Reserved
-  seekOffset =      BMPread32(bitmap);  // File Offset to Pixel Array
+  seekOffset =              BMPread32(bitmap);  // File Offset to Pixel Array
   BMPread32(bitmap);                    // skip DIB header size
-  w =               BMPread32(bitmap);  // Image Width
-  h =               BMPread32(bitmap);  // Image Height
+  w =                       BMPread32(bitmap);  // Image Width
+  h =                       BMPread32(bitmap);  // Image Height
   BMPread16(bitmap);                    // skip Plane
-  depth =           BMPread16(bitmap);  // Bit per Pixel
-  field =           depth / 8;          // Byte per Pixel
+  depth =                   BMPread16(bitmap);  // Bit per Pixel
+  field =                   depth / 8;          // Byte per Pixel
   //read32(bitmap);                       // skip Compression
 
   if (signature == 0x4D42 && ( (depth == 16) || (depth == 24) ) )
-  //if ( (depth == 16) || (depth == 24) )
-  { // casual Windows bitmap of 16 or 24 bit/pixel
-    uint8_t   lineBuffer[w * field];
-    uint16_t  padding =   (4 - ((w * field) & 3)) & 3;  // each bitmap row is n x 4 bytes
-    //uint8_t   lineBuffer[((w * field + 31) / 32) * 4]; // full row
-    //row size    =         ((w * field + 31) / 32) * 4;
-    //pixel array =         h * (((w * field + 31) / 32) * 4);
-    
-    y +=                  h - 1;  // bitmap starts with last line
-    //tft.setSwapBytes(true);       // swap color Bytes
-    //spr.setSwapBytes(true);       // swap color Bytes
+  { // legacy Windows bitmap and (16 or 24 bit/pixel), that's for us!!!
+    y +=                    h - 1;  // bitmap starts with last line
     bitmap.seek(seekOffset);      // go to Pixel Array
+    uint8_t   lineBuffer[w * field];
+    uint16_t  padding =     (4 - ((w * field) & 3)) & 3;  // each bitmap row is n x 4 bytes
+    // ((w * field + 31) / 32) * 4] // full row, // h * (((w * field + 31) / 32) * 4) // pixel array
+    
+    // Now separate 16 and 24 bit process to increase speed
+    if (depth == 24)
+    { // Convert 24 to 16 bit rgb(565) colours
+      for (row = 0; row < h; row++)
+      {
+        bitmap.read(lineBuffer, sizeof(lineBuffer));
+        uint8_t*  bptr =    lineBuffer;
+        uint16_t* tptr =    (uint16_t*)lineBuffer;
 
-    for (row = 0; row < h; row++)
-    {
-      bitmap.read(lineBuffer, sizeof(lineBuffer));
-      uint8_t*  bptr =    lineBuffer;
-      uint16_t* tptr =    (uint16_t*)lineBuffer;
-      
-      if (depth == 24)
-      { // Convert 24 to 16 bit colours
         for (uint16_t col = 0; col < w; col++)
         {
           b =       *bptr++;  g = *bptr++;  r = *bptr++;
-          //*tptr++ = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
-          rgb =     ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
-          *tptr++ = ((rgb & 0xFF00) >> 8) | ((rgb & 0x00FF) << 8); // swap color bytes
+          #ifndef BMP_SWAP_COLOR_BYTES  // MSB 1st
+          *tptr++ = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+          #else                         // swap, LSB 1st
+          *tptr++ = (r & 0xF8) | ((g & 0xE0) >> 5) | ((g & 0x1C) << 11) | ((b & 0xF8) << 5);
+          #endif  // BMP_SWAP_COLOR_BYTES
         }
+        // Push the pixel line to screen, pushImage will crop the line if needed
+        #if   (BMP_RECORD_TO == 0)
+        tft.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
+        #elif (BMP_RECORD_TO == 1)
+        spr.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
+        #elif (BMP_RECORD_TO == 2)
+        #endif // BMP_RECORD_TO
+      
+        if (padding)  bitmap.read((uint8_t*)lineBuffer, padding); // Read any byte padding to match next row
       }
-      else
-      { // Convert (555) to (565) 16 bit colours
+    }
+    else
+    { // Convert 16 bit colours rgb(555) to (565)
+      for (row = 0; row < h; row++)
+      {
+        bitmap.read(lineBuffer, sizeof(lineBuffer));
+        uint8_t*  bptr =    lineBuffer;
+        uint16_t* tptr =    (uint16_t*)lineBuffer;
+
         for (uint16_t col = 0; col < w; col++)
         {
           rgb =     *tptr;
-          //*tptr++ = ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x0200) >> 4) | ((rgb & 0x001F));
-          //rgb =   ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x0200) >> 4) | ((rgb & 0x001F));
-          //rgb =   ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x0100) >> 3) | ((rgb & 0x001F));
-          rgb =   ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x0080) >> 2) | ((rgb & 0x001F));
-          //rgb =   ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x0040) >> 1) | ((rgb & 0x001F));
-          //rgb =   ((rgb & 0x7C00) << 1) | ((rgb & 0x03E0) << 1) | ((rgb & 0x001F));
-          *tptr++ = ((rgb & 0xFF00) >> 8) | ((rgb & 0x00FF) << 8); // swap color bytes
+          #ifndef BMP_SWAP_COLOR_BYTES  // MSB 1st
+          *tptr++ = ((rgb & 0x7FE0) << 1) | ((rgb & 0x0200) >> 4) | ((rgb & 0x001F)); // G: b1 copied to b6
+          //*tptr++ = ((rgb & 0x7FE0) << 1) | ((rgb & 0x0100) >> 3) | ((rgb & 0x001F));
+          //*tptr++ = ((rgb & 0x7FE0) << 1) | ((rgb & 0x0080) >> 2) | ((rgb & 0x001F));
+          //*tptr++ = ((rgb & 0x7FE0) << 1) | ((rgb & 0x0040) >> 1) | ((rgb & 0x001F));
+          //*tptr++ = ((rgb & 0x7FE0) << 1) | (rgb & 0x0020)        | ((rgb & 0x001F)); // G: b5 copied to b6
+          #else                         // swap, LSB 1st
+          *tptr++ = ((rgb & 0x7F80) >> 7) | ((rgb & 0x0060) << 9) | ((rgb & 0x0200) << 4) | ((rgb & 0x001F) << 8); // G: b1 copied to b6
+          //*tptr++ = ((rgb & 0x7F80) >> 7) | ((rgb & 0x0060) << 9) | ((rgb & 0x0100) << 5) | ((rgb & 0x001F) << 8);
+          //*tptr++ = ((rgb & 0x7F80) >> 7) | ((rgb & 0x0060) << 9) | ((rgb & 0x0080) << 6) | ((rgb & 0x001F) << 8);
+          //*tptr++ = ((rgb & 0x7F80) >> 7) | ((rgb & 0x0060) << 9) | ((rgb & 0x0040) << 7) | ((rgb & 0x001F) << 8);
+          //*tptr++ = ((rgb & 0x7F80) >> 7) | ((rgb & 0x0060) << 9) | ((rgb & 0x0020) << 8) | ((rgb & 0x001F) << 8); // G: b5 copied to b6
+          #endif  // BMP_SWAP_COLOR_BYTES
         }
+        // Push the pixel line to screen, pushImage will crop the line if needed
+        #if   (BMP_RECORD_TO == 0)
+        tft.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
+        #elif (BMP_RECORD_TO == 1)
+        spr.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
+        #elif (BMP_RECORD_TO == 2)
+        #endif // BMP_RECORD_TO
+      
+        if (padding)  bitmap.read((uint8_t*)lineBuffer, padding); // Read any byte padding to match next row
       }
-      
-
-      // Push the pixel line to screen, pushImage will crop the line if needed
-      //tft.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
-      spr.pushImage(x, y--, w, 1, (uint16_t*)lineBuffer);
-      
-      //if (padding)  bitmap.read((uint8_t*)tptr, padding); // Read any byte padding
-      if (padding)  bitmap.read((uint8_t*)lineBuffer, padding); // Read any byte padding
     }
-    //tft.setSwapBytes(false);       // 
-    //spr.setSwapBytes(false);       //
     
-    //Serial.print("Loaded in "); Serial.print(millis() - startTime); Serial.println(" ms");
-    //spr.print("Loaded in "); spr.print(millis() - startTime); spr.println(" ms");
+    #ifdef BMP_SHOW_TIME
+    //Serial.printf("%s loaded in %d ms\n", filename, millis() - startTime);
+    #if   (BMP_RECORD_TO == 0)
+    tft.printf("%s loaded in %u ms\n", filename, millis() - startTime);
+    #elif (BMP_RECORD_TO == 1)
+    spr.printf("%s loaded in %u ms\n", filename, millis() - startTime);
+    #elif (BMP_RECORD_TO == 2)
+    #endif // BMP_RECORD_TO
+    #endif // BMP_SHOW_TIME
   }
   else
-  {
+  { // not a windows bitmap, not a 16 or 24 bit color 
     //Serial.println("BMP format not recognized");
+    #if   (BMP_RECORD_TO == 0)
+    tft.println("BMP format not recognized\n");
+    #elif (BMP_RECORD_TO == 1)
     spr.println("BMP format not recognized\n");
+    #elif (BMP_RECORD_TO == 2)
+    #endif // BMP_RECORD_TO
   }
   
   bitmap.close();
